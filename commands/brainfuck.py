a158c43fc8c97aa319fc6cfe2035e44b82497d6a
Sóla Lusøt <60041069+solaluset@users.noreply.github.com>
2024-05-23 22:54:22 +0300

    add Brainfuck command (#2)
    * start working on BF interpreter
    
    * continue working on BF interpreter
    
    * add BF to uk locale
    
    * add BF command
    
    * fix BF in uk locale
    
    * fix BF in uk locale
    
    * fix locales in BF
    
    * add BF to en locale
    
    * display message about empty result

---
+++ commands/brainfuck.py
@@ -0,0 +1,32 @@
+from i18n import t
+from discord.ext import commands
+
+from modules.brainfuck import BFException, State, parse
+
+
+MAX_OPS = 10_000
+
+
+@commands.command(usage="brainfuck.usage", aliases=("bf",))
+async def brainfuck(ctx, *, code: str):
+    "brainfuck.help"
+    if code.startswith("```"):
+        code = code.partition("\n")[2]
+        code, _, input_ = code.partition("```\n")
+    else:
+        input_ = ""
+    state = State(input_.encode(), MAX_OPS)
+    try:
+        state.interpret(parse(code))
+    except BFException as e:
+        await ctx.send(t("brainfuck.error", ctx.language, error=e))
+    else:
+        result = state.output.decode()
+        if result:
+            await ctx.send(t("brainfuck.result", ctx.language, result=result))
+        else:
+            await ctx.send(t("exec.completed_without_output", ctx.language))
+
+
+def setup(bot):
+    bot.add_command(brainfuck)
---
+++ modules/brainfuck.py
@@ -0,0 +1,111 @@
+from __future__ import annotations
+
+
+OP_INC = 0
+OP_DEC = 1
+OP_INC_PTR = 2
+OP_DEC_PTR = 3
+OP_WRITE = 4
+OP_READ = 5
+
+
+OPS = {
+    "+": OP_INC,
+    "-": OP_DEC,
+    ">": OP_INC_PTR,
+    "<": OP_DEC_PTR,
+    ".": OP_WRITE,
+    ",": OP_READ,
+}
+
+
+class BFException(Exception):
+    pass
+
+
+class Tree:
+    def __init__(self):
+        self.body = []
+        self.finished = False
+
+    def _branch_is_unfinished(self):
+        return (
+            self.body
+            and isinstance(self.body[-1], Tree)
+            and not self.body[-1].finished
+            and not self.finished
+        )
+
+    def append(self, part: int | Tree):
+        if self._branch_is_unfinished():
+            self.body[-1].append(part)
+        else:
+            self.body.append(part)
+
+    def finish(self):
+        if self._branch_is_unfinished():
+            self.body[-1].finish()
+        elif not self.finished:
+            self.finished = True
+        else:
+            raise BFException("unmatched ']'")
+
+
+def parse(code: str) -> Tree:
+    result = Tree()
+    for c in code:
+        if c in OPS:
+            result.append(OPS[c])
+        elif c == "[":
+            result.append(Tree())
+        elif c == "]":
+            result.finish()
+    result.finish()
+    if not result.finished:
+        raise BFException("'[' was never closed")
+    return result
+
+
+class State:
+    def __init__(self, input: bytes, ops_limit: int, mem_size: int = 30_000):
+        self.memory = bytearray(mem_size)
+        self.current_cell = 0
+        self.input = input
+        self.output = b""
+        self.ops_left = ops_limit
+
+    def decrement_ops(self):
+        self.ops_left -= 1
+        if self.ops_left == 0:
+            raise BFException("operation limit reached")
+
+    def interpret(self, code: Tree):
+        for op in code.body:
+            if isinstance(op, Tree):
+                while self.memory[self.current_cell]:
+                    self.interpret(op)
+                    self.decrement_ops()
+                continue
+            elif op == OP_INC:
+                try:
+                    self.memory[self.current_cell] += 1
+                except ValueError:  # overflow
+                    self.memory[self.current_cell] = 0
+            elif op == OP_DEC:
+                try:
+                    self.memory[self.current_cell] -= 1
+                except ValueError:  # overflow
+                    self.memory[self.current_cell] = 0xFF
+            elif op == OP_INC_PTR:
+                self.current_cell += 1
+                self.current_cell %= len(self.memory)
+            elif op == OP_DEC_PTR:
+                self.current_cell -= 1
+                self.current_cell %= len(self.memory)
+            elif op == OP_WRITE:
+                self.output += self.memory[self.current_cell : self.current_cell + 1]
+            elif op == OP_READ:
+                if self.input:
+                    self.memory[self.current_cell] = self.input[0]
+                    self.input = self.input[1:]
+            self.decrement_ops()
