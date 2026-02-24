18713144dbc012b4118ff7be238fc2c1e63c6dc6
solaluset <60041069+solaluset@users.noreply.github.com>
2026-02-24 15:43:22 +0200

    Replace scientific notation in calc command

--- commands/calc.py
+++ commands/calc.py
@@ -1,9 +1,15 @@
-from urllib.request import quote
+import re
+from decimal import Decimal
+from urllib.parse import quote
+
 from discord.ext import commands
+
 from modules import embed
 from modules.i18n import t
 
 ENDPOINT = "http://api.mathjs.org/v4/?expr="
+FLOAT = r"(\d+(\.\d*)?|\.\d+)"
+E_PATTERN = re.compile(rf"{FLOAT}e(\+|-)?\d+", re.I)
 
 
 @commands.command(usage="calc.usage")
@@ -14,7 +20,10 @@ async def calc(ctx, *, expr: str):
         async with ctx.bot.session.get(ENDPOINT + quote(expr)) as req:
             res = await req.text()
     except Exception:
-        res = t("calc.error", ctx.language)
+        return await ctx.send(t("calc.error", ctx.language))
+    res = E_PATTERN.sub(lambda m: f"{Decimal(m.group()):f}", res)
+    if len(res) > 2000:
+        res = res[:1997] + "..."
     em = embed.Embed(ctx, title=t("calc.result", ctx.language), description=res)
     await em.send()
 
